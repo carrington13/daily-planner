5e6ecac8b2213b761ecdb3aa27997ae083cb5209
Mediogre <caseyarrington13@gmail.com>
2021-03-21 12:19:02 -0500

    audit function and setInterval

--- assets/js/style.js
+++ assets/js/style.js
@@ -1,5 +1,4 @@
-//TODO
-// declare empty array to save tasks to
+// intialize global {tasks}
 let tasks = {
   nine: {},
   ten: {},
@@ -12,7 +11,6 @@ let tasks = {
   five: {},
 };
 // Save Task To Storage With Save button
-  // When Save Button Is Clicked
 const saveTask = function() {
   localStorage.setItem("tasks", JSON.stringify(tasks));
   
@@ -22,69 +20,109 @@ const saveTask = function() {
 const loadTasks = function () {
   // Convert storage back into usable data, push it to tasks
   tasks = JSON.parse(localStorage.getItem("tasks"));
-  
+  if (!tasks) {
+    tasks = {
+      nine: {time: 9,},
+      ten: {time: 10,},
+      eleven: {time: 11,},
+      twelve: {time: 12,},
+      one: {time: 13,},
+      two: {time: 14,},
+      three: {time: 15,},
+      four: {time: 16,},
+      five: {time: 17,}
+    }
+  };
+
   //loop over object properties
   for (const property in tasks) {
-    // debugger;
+    
     let divId = property;
     // find container el related to each property
     let containerEl = $("#" + divId);
     
     // find textarea child of containerEl
     let textareaEl = $(containerEl.find('textarea'));
-    console.log(textareaEl);
-    // set text as currentProperty.text value 
-    let text = tasks[property].text.trim();
-    console.log(text);
-    
-    $(textareaEl).text(text);
+    // set text as currentProperty.text value
+    if (tasks[property].text) { 
+      // set text as currentProperty.text value
+      let text = tasks[property].text.trim();
+      // set text of element to value of text variable
+      $(textareaEl).text(text);
+    }
   }
 }
 
 
 // Function Current Day
-  // Finds current Time of user
-  // Sets #currentDay.textContent to display current date
+let currentDay = function () {
+  // set now as current time for user
+  let now = dayjs();
+  // change format for display to user
+  let displayNow = now.format('dddd, MMMM D').toString();
+  // add formatted date to #currentDay in header
+  $('#currentDay').html(displayNow);
+  // get hour and send it to auditTask
+  let hour = dayjs().get('hour');
+  // send it to auditTask
+  auditHour(hour);
+}
 
-// Function Time/Interval
-  // Runs Hourly.
-  // If hour is passed hour
-    // Background-color: grey
+// Function to audit task by hour
+let auditHour = function (currentHour) {
+  for (const property in tasks) {
+    // find container el related to each property
+    let containerEl = $("#" + property);
+    // find textarea child of containerEl
+    let textareaEl = $(containerEl.find('textarea'));
+    // set text as currentProperty.text value 
+    let hourNow= tasks[property].time;
+
+    // !!! time property is set using 24-hour time, not 12 so function below will work !!!
+    // If hour is passed hour
+    if (hourNow < currentHour) {
+      // Background-color: grey
+      $(textareaEl).addClass('past');
     // Line through text
-  // else If Current Hour
-    // Background-color: red(danger)
-  // else If Next Hour
-    // Background-color: yellow(warning)
-  // else
-    // Background-color: green  
-// Function Add Text
-// $(".row").on("blur", "textarea", function(){
-//   //get textarea's current value/text
-//   var taskText = $(this)
-//   .val()
-//   .trim();
+    } else if (hourNow === currentHour) {
+      // Background-color: red(danger)
+      $(textareaEl).addClass('present');
+    } else {
+      // Background-color: green
+      $(textareaEl).addClass('future');
+    }
+  }
+}
 
-//   var status = $(this)
-//   .closest(".row")
-//   .attr("id");  
+// setInterval Function to audit hourly
+const hourInterval = setInterval (function() {
+  let time = dayjs().get('hour');
+  auditHour(time);
+}, (1000 * 60) * 60);
+
+// !!! Potentially Obsolete, further testing required.
+// // Task Creation and Editing
+// $(".row").on("click", "textarea", function(){
+//   // get current text from textarea
+//   var text = $(this).val().trim();
+  
+//   // create textarea el
+//   var textInput = $("<textarea>")
+//   .addClass("textarea col-10")
+//   .val(text);
+  
+//   // replace with <textarea>
+//   $(this).replaceWith(textInput);
+//   textInput.trigger("focus");
 // });
 
-// add and update text
-$(".row").on("click", "textarea", function(){
-  // get current text from textarea
-  var text = $(this).val().trim();
-  
-  // create textarea el
-  var textInput = $("<textarea>")
-  .addClass("textarea col-10 past")
-  .val(text);
-  
-  // replace with <textarea>
-  $(this).replaceWith(textInput);
-  textInput.trigger("focus");
-});
+// $(".row").on("click", "textarea", function () {
+//   var text = $(this).val().trim();
 
-$(".row").on("focusout", "textarea", function () {
+//   text.trigger('focus');
+// })
+
+$(".row").on("blur", "textarea", function () {
 
   // get <textarea> value
   var text = $(this).val().trim()
@@ -99,13 +137,10 @@ $(".row").on("focusout", "textarea", function () {
 })
 
 // eventHandler for SaveButtons 
-$('.saveBtn').on("click", function() {
-  // find id of hour task is located
-  // let hour = $(this)
-  //   .closest(".row")
-  //   .attr("id");
-    
+$('.saveBtn').on("click", function() {    
   saveTask();
 });
 
 loadTasks();
+currentDay();
+hourInterval();
